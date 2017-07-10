5024cc7f3084565b4034e2e5749d0f1743e4c25b
Masthanvali Shaik (UST <U52589@ust-global.com>
2017-07-10 16:09:59 +0530

    commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		System.out.println(" This is my first build from Jenkins !.... ");
 		System.out.println(" Hello Wold from Java !....");
 	}
 
